fa2b9fd90bb80ac96de93bf5e76addffabe98c14
quanldl <ldlq2005@gmail.com>
2026-04-14 00:18:53 +0700

    update infra dir

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "cloud-incident-system-tf-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "cloud-incident-system-terraform-locks"
+    encrypt        = true
+  }
+}
+
+
+
+
 # lambda_zip_path =  "../backend/lambda.zip"
 project_name = "cloud-incident-system"
 aws_region   = "us-east-1"
